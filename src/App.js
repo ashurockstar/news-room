7cfd302ba8c2cf3326c3f38c3ae0ba4e23ce53b7
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2021-12-29 22:10:21 +0530

    React-Router Implementation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <News
-          pageSize={3}
-          country="de"
-          category="business" />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" ><News key="general" pageSize={3} country="in"category="general" /></Route>
+            <Route exact path="/business" ><News key="business" pageSize={3} country="in" category="business" /></Route>
+            <Route exact path="/entertainment" ><News key="entertainment" pageSize={3} country="in" category="entertainment" /></Route>
+            <Route exact path="/general" ><News key="general" pageSize={3} country="in" category="general" /></Route>
+            <Route exact path="/health" ><News key="health" pageSize={3} country="in" category="health" /></Route>
+            <Route exact path="/science" ><News key="science" pageSize={3} country="in" category="science" /></Route>
+            <Route exact path="/sports" ><News key="sports" pageSize={3} country="in" category="sports" /></Route>
+            <Route exact path="/technology" ><News key="technology" pageSize={3} country="in" category="technology" /></Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
